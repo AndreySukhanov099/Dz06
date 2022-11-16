b3b77d8757e3efb3fa522206080d631bdf4c44fe
And_Sukhanov <Andrey.sx@mail.ru>
2022-11-16 21:10:50 +0300

    Задача 43, добавил случай, когда прямые совпадают

---
+++ 41/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+int[] NewArray(int[] array)
+{
+    for(int i =0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i =0; i < array.Length; i ++)
+    {
+        if(array[i] > 0)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите размер массива");
+int number = int.Parse(Console.ReadLine()!);
+if(number > 0)
+{
+    int[] array = new int[number];
+    array = NewArray(array);
+    Console.WriteLine("["+ string.Join(", ", array) +"]");
+    Console.WriteLine("Число положительных чисел: " + PositiveNumbers(array));
+}
+else
+Console.WriteLine("Неверный размер массива");
---
+++ 43/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Введите число b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b2");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число k2");
+double k2 = double.Parse(Console.ReadLine()!);
+if (k1 != k2)
+    {
+        double y = (((b2 * k1) - (b1 * k2)) / (k1 - k2));
+        if(k1 != 0)
+        {
+            double x = (y - b1) / k1;
+            Console.WriteLine("координата x: " + x);
+            Console.WriteLine("координата y: " + y);
+        }
+        else
+        {
+            double x = (y - b2) / k2;
+            Console.WriteLine("координата x: " + x);
+            Console.WriteLine("координата y: " + y);
+        }
+    }  
+else
+{   
+    if(b1 != b2)
+    {
+        Console.WriteLine("Точек пересечения нет");
+    }
+    else
+    {
+        Console.WriteLine("Прямые совпадают(бесконечное количество точек пересечений");
+    } 
+}
